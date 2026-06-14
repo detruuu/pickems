caa618f1d3f3d8fe99885e89a281109eee99f995
detruuu <91836@student.ue.poznan.pl>
2026-06-14 06:34:46 +0200

    PORT SIX SEVENNNNNN

--- src/server.js
+++ src/server.js
@@ -9,7 +9,7 @@ const { init } = require('./db');
 const { syncConfiguredUsers } = require('./auth_config');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 6767;
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
